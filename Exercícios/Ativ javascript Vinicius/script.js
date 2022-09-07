8fd999d1b144eb0c041c0652164c03db1cc693af
Vinicius31Vitor18 <105638478+vinicius18vitor31@users.noreply.github.com>
2022-09-07 16:12:55 -0300

    Atualizando

---
+++ Exercícios/Ativ javascript Vinicius/script.js
@@ -0,0 +1,17 @@
+
+/*ESta forma para testar se p javascript está lincado corretamente, no inspecioar tem que ir em Console. O script so funciona ápos o que ja foi digitado. 
+console.log(Teste);
+*/
+
+/*
+const nome = "JavaScript";
+
+console.log(nome);
+*/
+
+const nav = document.querySelector ("nav");
+const produtos = document.querySelector (".produtos a");
+
+console.log(nav);
+
+console.log(produtos);
