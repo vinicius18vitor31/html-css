11884d246b9893d0e7da81424b9476caf1975e18
Vinicius31Vitor18 <105638478+vinicius18vitor31@users.noreply.github.com>
2022-09-12 19:27:34 -0300

    Atualizando Atividade

---
+++ Exercícios/Ativ javaScript 06/script.js
@@ -0,0 +1,10 @@
+
+const ativar = document.querySelector('.ativar');
+
+function ativarAoClick(event) {
+    console.log(event);
+}
+
+ativar.addEventListener('click', ativarAoClick)
+
+console.log(ativar);
