a0e3ea14c0c3b8afb2c13b7b89d56f4e74596e97
Vinicius31Vitor18 <105638478+vinicius18vitor31@users.noreply.github.com>
2022-09-12 16:50:44 -0300

    Continua

--- Exercícios/Ativ javaScript 01/script.js
+++ Exercícios/Ativ javaScript 01/script.js
@@ -13,5 +13,14 @@ const nav = document.querySelector ("nav");
 const produtos = document.querySelector (".produtos a");
 
 console.log(nav);
-
 console.log(produtos);
+
+console.log(produtos.href);
+
+console.dir(nav);
+
+nav.style.backgroundColor = "black";
+nav.style.padding = "20px";
+
+nav.classList.add("ativos");
+
---
+++ Exercícios/Ativ javaScript 02/script.js
@@ -0,0 +1,32 @@
+/*Usando String textos*/
+const text = "JavaScript";
+
+const frase = 'Isso é o meu "jogo"';
+
+console.log(text);
+console.log(frase);
+
+console.log(text + ' ' + frase);
+
+/*Number, números*/
+
+let total = 0;
+
+console.log(total);
+
+const compras = 30;
+
+const imposto = 10;
+
+total = compras + imposto;
+
+console.log(total);
+/*para multiplicar coloca *; ex: const dobro = total * 2; Para dividir coloca /; ex: const dividir = total / 2; Para diminuir coloca -; ex: const diminuir = total - 2 */ 
+
+const String = "20" + "20";
+console.log(String);
+
+const Number = 20 + 20;
+console.log(Number);
+
+/*Para transformar uma string em número; const transformar = ("20") + 20;*/
