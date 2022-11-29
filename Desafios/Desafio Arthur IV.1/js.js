afca881f1db4ed01690be9fd3e6d4b3fef284470
Vinicius dos Santos de Souza <vinicius01vitor@hotmail.com>
2022-11-29 17:46:51 -0300

    Atualizando

---
+++ Desafios/Desafio Arthur IV.1/js.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+    var botao = $('.hamburguer');
+    var dropDown = $('.itens');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.expandir-curso');
+    var dropDown = $('.menu');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
